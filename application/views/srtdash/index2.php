4ccbd17e60876f6635da9e80da2dda94675faf68
JPTiu <crashantrax0@gmail.com>
2019-03-24 01:38:31 -0400

    Updated 24-19

--- application/views/srtdash/index2.php
+++ application/views/srtdash/index2.php
@@ -342,7 +342,7 @@
                     <div class="col-sm-6 clearfix">
                         <div class="user-profile pull-right">
                             <img style="border-radius: 50%;border:1px solid black;padding: 5px;" class="avatar user-thumb" src="public/contents-css/logo/admin.png" alt="avatar">
-                            <h4 class="user-name dropdown-toggle" data-toggle="dropdown"> Admin <i class="fa fa-angle-down"></i></h4>
+                            <h4 class="user-name dropdown-toggle" data-toggle="dropdown"> Manager <i class="fa fa-angle-down"></i></h4>
                             <div class="dropdown-menu">
                                 <a class="dropdown-item" href="<?= base_url(); ?> ">Message</a>
                                 <a class="dropdown-item" href="<?= base_url(); ?> ">Settings</a>
