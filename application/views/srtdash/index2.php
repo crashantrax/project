7ced23cbba26de0e172b0898cfda83abe9422223
JPTiu <crashantrax0@gmail.com>
2019-03-25 09:31:17 -0400

    3-25 Lag free

--- application/views/srtdash/index2.php
+++ application/views/srtdash/index2.php
@@ -13,8 +13,6 @@
     <link rel="stylesheet" href="<?= base_url('assets/css/metisMenu.css'); ?> ">
     <link rel="stylesheet" href="<?= base_url('assets/css/owl.carousel.min.css'); ?> ">
     <link rel="stylesheet" href="<?= base_url('assets/css/slicknav.min.css'); ?> ">
-    <!-- amchart css -->
-    <link rel="stylesheet" href="<?= base_url('https://www.amcharts.com/lib/3/plugins/export/export.css');?>" type="text/css" media="all" />
     <!-- others css -->
     <link rel="stylesheet" href="<?= base_url('assets/css/typography.css'); ?> ">
     <link rel="stylesheet" href="<?= base_url('assets/css/default-css.css'); ?> ">
@@ -373,17 +371,6 @@
     <script src="assets/js/metisMenu.min.js"></script>
     <script src="assets/js/jquery.slimscroll.min.js"></script>
     <script src="assets/js/jquery.slicknav.min.js"></script>
-
-    <!-- start chart js -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.min.js"></script>
-    <!-- start highcharts js -->
-    <script src="https://code.highcharts.com/highcharts.js"></script>
-    <!-- start zingchart js -->
-    <script src="https://cdn.zingchart.com/zingchart.min.js"></script>
-    <script>
-    zingchart.MODULESDIR = "https://cdn.zingchart.com/modules/";
-    ZC.LICENSE = ["569d52cefae586f634c54f86dc99e6a9", "ee6b7db5b51705a13dc2339db3edaf6d"];
-    </script>
     <!-- all line chart activation -->
     <script src="assets/js/line-chart.js"></script>
     <!-- all pie chart -->
--- application/views/srtdash/index3.php
+++ application/views/srtdash/index3.php
@@ -306,13 +306,6 @@
     <script src="assets/js/metisMenu.min.js"></script>
     <script src="assets/js/jquery.slimscroll.min.js"></script>
     <script src="assets/js/jquery.slicknav.min.js"></script>
-
-    <!-- start chart js -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.min.js"></script>
-    <!-- start highcharts js -->
-    <script src="https://code.highcharts.com/highcharts.js"></script>
-    <!-- start zingchart js -->
-    <script src="https://cdn.zingchart.com/zingchart.min.js"></script>
     
     <!-- all line chart activation -->
     <script src="assets/js/line-chart.js"></script>
