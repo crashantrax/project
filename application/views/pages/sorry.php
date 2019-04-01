80285fbab5dadf66a7894f18a5fa1f31ae26672e
unknown <kentpaulmadrid28@gmail.com>
2019-04-02 01:23:23 +0800

    02/04/19 Sorry browser doesn't exist

---
+++ application/views/pages/sorry.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+  <link rel="stylesheet" href="<?= base_url('assets/css/sorry.css')?>">
+</head>
+
+
+<body>
+<div id="app">
+   <div>SORRY</div>
+   <div class="txt">
+      BROWSER DOESN'T EXIST<span class="blink">_</span>
+   </div>
+</div>
+</body>
+</html>
